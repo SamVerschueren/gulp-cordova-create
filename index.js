26364a93af34afd178aa8a0747073c4e8b02a162
Sam Verschueren <sam.verschueren@gmail.com>
2015-04-23 12:07:32 +0200

    Used cordova.raw instead of cordova because the raw cordova returns promises so the Q library can be removed

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ var path = require('path'),
     mkdirp = require('mkdirp'),
     through = require('through2'),
     gutil = require('gulp-util'),
-    Q = require('q'),
-    cordovaLib = require('cordova-lib'),
-    cordova = cordovaLib.cordova;
+    cordova = require('cordova-lib').cordova.raw;
 
 module.exports = function(options) {
 
@@ -23,6 +21,9 @@ module.exports = function(options) {
 
     return through.obj(function(file, enc, cb) {
         if(!file.isDirectory()) {
+            // Quit if the file provided is not a directory. In the future we can maybe
+            // store all the files in a temporary directory and pass the temporary directory
+            // to the cordova create command
             cb(new gutil.PluginError('gulp-cordova-create', 'You can only pass in a folder.'));
             return;
         }
@@ -31,11 +32,11 @@ module.exports = function(options) {
             dir = '.cordova',
             config = {lib: {www: {url: file.path}}};
 
+        // Make sure the first make the directory recursively
         mkdirp(dir, function() {
-            Q.fcall(function() {
-                cordova.create(dir, options.id, options.name, config);
-            })
-            .then(function() {
+            // Create the cordova project in the correct directory
+            cordova.create(dir, options.id, options.name, config).then(function() {
+                // Pass in the cordova project directory to the next step
                 self.push(new gutil.File({
                     base: file.cwd,
                     cwd: file.cwd,
@@ -43,6 +44,7 @@ module.exports = function(options) {
                     stat: fs.statSync(path.join(file.cwd, dir))
                 }));
 
+                // Continue
                 cb();
             });
         });
