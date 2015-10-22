90fb5b73e67d8425a973923de3bc52b7a8bb235d
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-22 12:08:43 +0200

    Refactored implementation

--- index.js
+++ index.js
@@ -3,58 +3,55 @@
 /**
  * Creates a new cordova project in the current directory.
  *
- * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @author Sam Verschueren	  <sam.verschueren@gmail.com>
  * @since  18 April 2015
  */
 
 // module dependencies
-var path = require('path'),
-    fs = require('fs'),
-    mkdirp = require('mkdirp'),
-    through = require('through2'),
-    gutil = require('gulp-util'),
-    cordova = require('cordova-lib').cordova.raw;
+var path = require('path');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var through = require('through2');
+var gutil = require('gulp-util');
+var cordova = require('cordova-lib').cordova.raw;
 
-module.exports = function(options) {
+module.exports = function (options) {
+	var firstFile;
 
-    var firstFile;
+	return through.obj(function (file, enc, cb) {
+		if (!file.isDirectory()) {
+			// Quit if the file provided is not a directory. In the future we can maybe
+			// store all the files in a temporary directory and pass the temporary directory
+			// to the cordova create command
+			cb(new gutil.PluginError('gulp-cordova-create', 'You can only pass in a folder.'));
+			return;
+		}
 
-    return through.obj(function(file, enc, cb) {
-        if(!file.isDirectory()) {
-            // Quit if the file provided is not a directory. In the future we can maybe
-            // store all the files in a temporary directory and pass the temporary directory
-            // to the cordova create command
-            cb(new gutil.PluginError('gulp-cordova-create', 'You can only pass in a folder.'));
-            return;
-        }
+		if (firstFile === undefined) {
+			options = options || {};
 
-        if(firstFile === undefined) {
-            firstFile = file;
-        }
+			var self = this;
+			var dir = options.dir || '.cordova';
+			var config = {lib: {www: {url: firstFile.path}}};
 
-        cb();
-    }, function(cb) {
-        options = options || {};
+			// Make sure the directory exists
+			mkdirp(dir, function () {
+				// Create the cordova project in the correct directory
+				cordova.create(dir, options.id, options.name, config).then(function () {
+					// Pass in the cordova project directory to the next step
+					self.push(new gutil.File({
+						base: firstFile.cwd,
+						cwd: firstFile.cwd,
+						path: path.join(firstFile.cwd, dir),
+						stat: fs.statSync(path.join(firstFile.cwd, dir))
+					}));
 
-        var self = this,
-            dir = options.dir || '.cordova',
-            config = {lib: {www: {url: firstFile.path}}};
-
-        // Make sure the directory exists
-        mkdirp(dir, function() {
-            // Create the cordova project in the correct directory
-            cordova.create(dir, options.id, options.name, config).then(function() {
-                // Pass in the cordova project directory to the next step
-                self.push(new gutil.File({
-                    base: firstFile.cwd,
-                    cwd: firstFile.cwd,
-                    path: path.join(firstFile.cwd, dir),
-                    stat: fs.statSync(path.join(firstFile.cwd, dir))
-                }));
-
-                // Continue
-                cb();
-            }).catch(cb);
-        });
-    });
+					// Continue
+					cb();
+				}).catch(function (err) {
+					cb(new gutil.PluginError('gulp-cordova-create', err.message));
+				});
+			});
+		}
+	});
 };
